831a490ec97b3f932ebda98618c988290e362c11
Marco Di Summa <mdisumma@yahoo.com>
2025-10-06 13:54:51 +0100

    feat: refactor ResultStatus component and update ResultScreen layout with buttons

--- screens/ResultScreen.tsx
+++ screens/ResultScreen.tsx
@@ -1,8 +1,8 @@
-import ResultStatus from "@/components/inspection/result/ResultStatus";
+import ResultStatus from "@/components/result/ResultStatus";
 import { RootStackParamList } from "@/types/navigation";
 import { StackNavigationProp } from "@react-navigation/stack";
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { Button, ScrollView, StyleSheet, View } from "react-native";
 
 type ResultScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -16,12 +16,35 @@ interface Props {
 export default function ResultScreen({ navigation }: Props) {
   return (
     <ScrollView style={styles.container}>
-      <ResultStatus resultTitle="Visual Inspection" resultStatus="Passed" />
-      <ResultStatus
-        resultTitle="Electrical Safety Test"
-        resultStatus="Passed"
-      />
-      <ResultStatus resultTitle="Functional Inspection" resultStatus="Passed" />
+      <View style={styles.contentContainer}>
+        <View>
+          <ResultStatus resultTitle="Visual Inspection" resultStatus="Passed" />
+          <ResultStatus
+            resultTitle="Electrical Safety Test"
+            resultStatus="Passed"
+          />
+          <ResultStatus
+            resultTitle="Functional Inspection"
+            resultStatus="Passed"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+              title="Add label"
+              color="#F4F8FC"
+              onPress={() => console.log("Add label button pressed")}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Save Inspection"
+              color="#F4F8FC"
+              onPress={() => console.log("Save Inspection button pressed")}
+            />
+          </View>
+        </View>
+      </View>
     </ScrollView>
   );
 }
@@ -30,6 +53,23 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
     color: "#333",
-    padding: 24,
+    padding: 16,
+    paddingTop: 32,
+  },
+  contentContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    minHeight: "100%",
+  },
+  buttonContainer: {
+    marginBottom: 32,
+    gap: 16,
+  },
+  button: {
+    backgroundColor: "#142C44",
+    width: "100%",
+    borderRadius: 16,
+    overflow: "hidden",
   },
 });
